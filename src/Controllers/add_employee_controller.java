3ce1b9366d83de65159997d7240d27e688bc2301
hasank18 <kesserwan.hasan.18@gmail.com>
2018-12-28 00:05:51 +0200

    modify login little modifications

--- src/Controllers/add_employee_controller.java
+++ src/Controllers/add_employee_controller.java
@@ -1,4 +1,18 @@
 package Controllers;
 
-public class add_employee_controller {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class add_employee_controller implements Initializable {
+    @FXML
+    ChoiceBox<String> choiceBox;
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        choiceBox.getItems().addAll("Male","Female");
+
+    }
 }
