b5344328da26b615e582437f949b7bd913509036
hasank18 <kesserwan.hasan.18@gmail.com>
2018-12-29 12:03:14 +0200

    edit

--- src/Controllers/Lib_Controller.java
+++ src/Controllers/Lib_Controller.java
@@ -1,5 +1,6 @@
 package Controllers;
 
+import javafx.beans.property.ReadOnlyStringWrapper;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -9,9 +10,22 @@ import javafx.scene.control.*;
 import javafx.event.ActionEvent;
 import javafx.scene.layout.HBox;
 import java.net.URL;
+import java.sql.*;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class Lib_Controller implements Initializable {
+    String server="localhost";
+    int port=3306;
+    String user="root";
+    String password="";
+    String database="mydb";
+    String jdbcurl;
+    Connection con=null;
+    @FXML
+    TableView<String> table;
+    @FXML
+    TextField book_name_field;
     @FXML
     HBox container;
     @FXML
@@ -65,6 +79,37 @@ public class Lib_Controller implements Initializable {
         container.getChildren().addAll(label,choiceBox);
         container.setMargin(label,new Insets(0,8,0,0));
         container.setMargin(choiceBox,new Insets(0,0,0,8));
+
+    }
+    @FXML
+    private void handleSearchEvent(ActionEvent event){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con= DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb","root","" + "" );
+            Statement stmt=con.createStatement();
+            String test = "select *from Books where BookName='"+book_name_field.getText()+"'";
+            ResultSet rs2 = stmt.executeQuery(test);
+            rs2.next();
+            System.out.println(rs2.getFetchSize());
+            System.out.println(rs2.getInt(1));
+            table.getColumns().removeAll(table.getColumns());
+            TableColumn<String,String> col1= new TableColumn<String, String>("ID");
+            col1.setCellValueFactory(data -> {
+                String Value = data.getValue();
+                return new ReadOnlyStringWrapper(Value);
+            });
+            table.getColumns().add(col1);
+            String string = ""+rs2.getInt(1);
+            table.getItems().addAll(string,string,string,"hasan");
+
+
+
+        }catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+
 
     }
 }
