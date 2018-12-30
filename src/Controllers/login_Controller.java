10e9469d4b8c8080b76195b2af4c6c925fbe4edf
hasank18 <kesserwan.hasan.18@gmail.com>
2018-12-30 17:20:59 +0200

    edit

--- src/Controllers/login_Controller.java
+++ src/Controllers/login_Controller.java
@@ -60,7 +60,7 @@ public class login_Controller implements Initializable {
         String UserName ="";
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            con= DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb","hanin","h@n!nabbas123" + "" );
+            con= DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb","root","" + "" );
             pst = con.prepareStatement("select UserName from Employee where UserName = ?");
             pst.setString(1, name.getText());
             rs = pst.executeQuery();
@@ -79,7 +79,7 @@ public class login_Controller implements Initializable {
         String Password ="";
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            con= DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb","hanin","h@n!nabbas123" + "" );
+            con= DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb","root","" + "" );
             pst = con.prepareStatement("select Password from Employee where Password = ?");
             pst.setString(1, pass.getText());
             rs = pst.executeQuery();
