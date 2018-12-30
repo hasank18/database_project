bb6d02e58d43056be46bf97c1acab569b0ab3de9
hasank18 <kesserwan.hasan.18@gmail.com>
2018-12-30 03:10:35 +0200

    edit

--- src/Controllers/login_Controller.java
+++ src/Controllers/login_Controller.java
@@ -1,6 +1,5 @@
 package Controllers;
 
-import com.mysql.cj.protocol.ResultsetRow;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -10,26 +9,20 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.stage.Stage;
-import sample.DBconnection;
-import sun.security.util.Password;
-
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.PreparedStatement;
 import java.awt.*;
-import java.io.IOException;
-import java.net.URL;
-import java.sql.Connection;
 import java.util.ResourceBundle;
 
 public class login_Controller implements Initializable {
 
     String server = "localhost";
     int port = 3306;
-    String user = "hanin";
-    String password = "h@n!nabbas123";
+    String user = "root";
+    String password = "";
     String database = "mydb";
     String jdbcurl;
     Connection con = null;
@@ -38,13 +31,13 @@ public class login_Controller implements Initializable {
 
 
     @FXML
-    private TextField name;
+    private javafx.scene.control.TextField name;
     @FXML
     private PasswordField pass;
     @FXML
     private Button btnLogin;
     @FXML
-    private Label showresult;
+    private javafx.scene.control.Label showresult;
 
 
     @Override
@@ -56,7 +49,7 @@ public class login_Controller implements Initializable {
         String UserName ="";
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            con= DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb","hanin","h@n!nabbas123" + "" );
+            con= DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb","root","" + "" );
             pst = con.prepareStatement("select UserName from Employee where UserName = ?");
             pst.setString(1, name.getText());
             rs = pst.executeQuery();
@@ -75,7 +68,7 @@ public class login_Controller implements Initializable {
         String Password ="";
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            con= DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb","hanin","h@n!nabbas123" + "" );
+            con= DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb","root","" + "" );
             pst = con.prepareStatement("select Password from Employee where Password = ?");
             pst.setString(1, pass.getText());
             rs = pst.executeQuery();
@@ -92,19 +85,21 @@ public class login_Controller implements Initializable {
 
     @FXML
     private void eventHandler(ActionEvent event) throws Exception {
-        if (name.getText().equals(getUserName()) && pass.getText().equals(getPassword())) {
+        try {
+            if (name.getText().equals(getUserName()) && pass.getText().equals(getPassword())) {
 
-            showresult.setText("welcome");
-            Parent parent = FXMLLoader.load(getClass().getResource("../fxml_files/main_page2.fxml"));
-            Scene scene = new Scene(parent);
-            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-            stage.setScene(scene);
+                showresult.setText("welcome");
+                Parent parent = FXMLLoader.load(getClass().getResource("../fxml_files/main_page2.fxml"));
+                Scene scene = new Scene(parent);
+                Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                stage.setScene(scene);
 
-            stage.show();
+                stage.show();
 
-        } else {
+            }
+        }catch (Exception e ){
             showresult.setText("failed to login");
         }
 
     }
-}//check getuser getpass
+}
